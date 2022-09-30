69a12e8b6cd7713862672f5f66edb04d245ca345
Antwaun Johnson <Antwaun.L.Johnson@gmail.com>
2022-09-30 08:30:05 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BugTrackerPro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
