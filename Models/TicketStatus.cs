11ec5c43db03225d91c5a7cb82189e63562b4f0c
Antwaun Johnson <Antwaun.L.Johnson@gmail.com>
2022-10-02 21:18:32 -0700

    created models for TicketPriority, TicketStatus, and TicketType

---
+++ Models/TicketStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace BugTrackerPro.Models;
+
+public class TicketStatus
+{
+    public int Id { get; set; }
+
+    [DisplayName("Status Name")]
+    public string? Title { get; set; }
+}
---
+++ Models/TicketType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace BugTrackerPro.Models;
+
+public class TicketType
+{
+    public int Id { get; set; }
+
+    [DisplayName ("Type Name")]
+    public string? Name { get; set; }
+}
